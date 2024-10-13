34259ac7bf0646ef74ce09226077d70f2c09acb2
Mateusz Mlodecki <mateuszmlodecki@icloud.com>
2024-10-13 11:24:40 +0200

    added gallery

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -1,15 +1,15 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom';
-import logo from '../images/logo.png'
-import menuPng from '../images/menu.png'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
+import menuPng from "../images/menu.png";
 
 export const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-    
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="grid grid-cols-5 xl:grid-cols-6 grid-rows-1 xl-grid-rows-2 items-center">
       <img
@@ -17,46 +17,58 @@ export const Navbar = () => {
         alt="logo"
         className="col-start-1 col-span-2 md:col-span-1"
       />
-      <nav className='hidden md:flex'>
+      <nav className="hidden md:flex">
         <div>
-            <Link to="/Balonowa" className="button1">
+          <Link to="/Balonowa" className="button1">
             <b>HOME</b>
-            </Link>
+          </Link>
         </div>
         <div>
-            <Link to="/Oferta" className='button2'>
+          <Link to="/Oferta" className="button2">
             <b>OFERTA</b>
-            </Link>
+          </Link>
         </div>
         <div>
-            <Link to="/Kontakt" className='button1'>
-            <b>Kontakt</b>
-            </Link>
+          <Link to="/Kontakt" className="button1">
+            <b>KONTAKT</b>
+          </Link>
         </div>
         <div>
-            <Link to="/Galeria"className='button2'>
+          <Link to="/Galeria" className="button2">
             <b>GALERIA</b>
-            </Link>
+          </Link>
         </div>
       </nav>
-<img src={menuPng} alt="Menu" className='hamburger col-start-5 justify-self-end coursor-pointer md:hidden' onClick={toggleMenu} />
-{menuOpen && (
-    <nav className='col-start-3 row-start-1'>
-        <div>
-            <Link to="/Balonowa" className='button1' onClick={toggleMenu}><b>HOME</b></Link>
-        </div>
-        <div>
-            <Link to="/Oferta" className='button2' onClick={toggleMenu}><b>OFERTA</b></Link>
-        </div>
-        <div>
-            <Link to="/Kontakt" className='button1' onClick={toggleMenu}><b>KONTAKT</b></Link>
-        </div>
-        <div>
-            <Link to="/Galeria" className='button2' onClick={toggleMenu}><b>GALERIA</b></Link>
-        </div>
-    </nav>
-    
-)}
-      </div>
-  )
-}
+      <img
+        src={menuPng}
+        alt="Menu"
+        className="hamburger col-start-5 justify-self-end coursor-pointer md:hidden"
+        onClick={toggleMenu}
+      />
+      {menuOpen && (
+        <nav className="col-start-3 row-start-1">
+          <div>
+            <Link to="/Balonowa" className="button1" onClick={toggleMenu}>
+              <b>HOME</b>
+            </Link>
+          </div>
+          <div>
+            <Link to="/Oferta" className="button2" onClick={toggleMenu}>
+              <b>OFERTA</b>
+            </Link>
+          </div>
+          <div>
+            <Link to="/Kontakt" className="button1" onClick={toggleMenu}>
+              <b>KONTAKT</b>
+            </Link>
+          </div>
+          <div>
+            <Link to="/Galeria" className="button2" onClick={toggleMenu}>
+              <b>GALERIA</b>
+            </Link>
+          </div>
+        </nav>
+      )}
+    </div>
+  );
+};
--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Grid } from "@mui/material";
+
+const images = require.context("../Gallery", true, /\.(jpg|jpeg)$/);
 
 const Galeria = () => {
-  return (
-    <div>Galeria</div>
-  )
-}
+  const [enlargedImage, setEnlargedImage] = useState(null);
 
-export default Galeria
+  const handleImageClick = (image) => {
+    setEnlargedImage(image);
+  };
+
+  const handleClose = () => {
+    setEnlargedImage(null);
+  };
+
+  return (
+    <Grid container spacing={2}>
+      {images.keys().map((image, index) => (
+        <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+          <img
+            src={images(image)}
+            alt="Zdjęcie"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              cursor: "pointer", // add a pointer cursor on hover
+            }}
+            onClick={() => handleImageClick(image)}
+          />
+          {enlargedImage === image && (
+            <div
+              style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                zIndex: "99999",
+              }}
+              onClick={handleClose}
+            >
+              <img
+                src={images(image)}
+                alt="Zdjęcie"
+                style={{
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                  objectFit: "contain",
+                }}
+              />
+            </div>
+          )}
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default Galeria;
